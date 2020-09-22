67b6ca2cf88877d9378d227590cd5d1ac4cf0485
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-22 21:48:44 +0530

    5th commit

--- Employee.java
+++ Employee.java
@@ -8,19 +8,19 @@ public class Employee
 		int part_time=0;
 		int emp_wage_hr=20;
 		int emp_hr=0;
+		int total_hr=0;
+		int emp_days=20;
 		int emp_wage=0;
-		double number=Math.floor(Math.random()*10)%2;
-		int val= (int)number;
-		switch(val)
+		for(int i=0;i<emp_days;i++)
 		{
-	        case 0:
-	        	emp_hr=4;
-	        	break;
-	        case 1:
-	        	emp_hr=8;
-	        	break;
-		}
-		emp_wage=emp_hr*emp_wage_hr;
+			double number=Math.floor(Math.random()*10)%2;
+			if(full_time== number)
+			     emp_hr=8;
+			else if(part_time== number)
+				emp_hr=4;
+			total_hr+=emp_hr;
+		}	
+		emp_wage=total_hr*emp_wage_hr;
 		System.out.println("Employee wage: "+emp_wage);
 	}
 }
