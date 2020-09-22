11272bd8284ecbeac3fac2eddd5f94107553b1b3
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-22 19:42:23 +0530

    New commit

--- Employee.java
+++ Employee.java
@@ -4,16 +4,6 @@ public class Employee
 {
 	public static void main(String[] args)
 	{
-		int full_time=1;
-		int emp_wage_hr=20;
-		int emp_hr=0;
-		int emp_wage=0;
-		double number=Math.floor(Math.random()*10)%2;
-		if(full_time== number)
-		     emp_hr=8;
-		else
-			emp_hr=0;
-		emp_wage=emp_hr*emp_wage_hr;
-		System.out.println("Employee wage: "+emp_wage);
+		System.out.println("Welcome to Employee Wage Computation");
 	}
 }
