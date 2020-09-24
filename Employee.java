1475ffc60633e788083ebd167d406e70299b9ff1
sumitsamsharma <43275389+sumitsamsharma@users.noreply.github.com>
2020-09-24 10:57:39 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -6,18 +6,21 @@ public class Employee
 	{
 		int full_time=1;
 		int part_time=0;
+		int absent=2;
 		int emp_wage_hr=20;
 		int emp_hr=0;
 		int total_hr=0;
 		int emp_days=0;
 		int emp_wage=0;
-		while((emp_days<20) && total_hr<100)
+		while((emp_days<20) && total_hr<=100)
 		{
-			double number=Math.floor(Math.random()*10)%2;
+			double number=Math.floor(Math.random()*10)%3;
 			if(full_time== number)
 			     emp_hr=8;
 			else if(part_time== number)
 				emp_hr=4;
+			else
+				emp_hr=0;
 			total_hr+=emp_hr;
 			emp_days++;
 		}	
