d2caaaf0711502a755963136a9e35b31092512be
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-30 12:18:31 +0530

    Managing multiple wage of comapnies

--- Employee.java
+++ Employee.java
@@ -1,72 +1,64 @@
 package Emp_info;
 
-class Employee_func
+
+public class Employee
 {
 	    public static final int full_time=1;
 	    public static final int part_time=0;
-	    private int emp_wage_hr;
-	    private int total_no_days;
-	    public int total_hr=0;
-	    int emp_hours;
-	    public int get_total_wage()  
-	    { 
-	    	int emp_wage=total_hr*emp_wage_hr;
-	    	return emp_wage;
+	   
+	    int total_companies=0;
+	    private CompanyEmpWage[]  company_emp_wage;
+	    
+	    private void add_CompanyWage(String company, int emp_wage_hr, int total_no_days, int total_hr) 
+	    {
+	        company_emp_wage[total_companies] = new CompanyEmpWage(company,emp_wage_hr, total_no_days,total_hr); 
+	        total_companies++; 
 	    } 
-	    public int get_total_hours()  
-	    { 
-	    	int emp_hr=0;
-	    	int days=0;
+	    private void get_total_wage()
+	    {
+	         for (int i = 0; i < total_companies; i++) 
+	         { 
+	        	 company_emp_wage[i].setEmpWage(this.get_total_wage(company_emp_wage[i])); 
+	        	 System.out.println(company_emp_wage[i]); 
+	         }
+	    }
+	    
+	    public Employee()
+	    {
+	    	company_emp_wage= new CompanyEmpWage[4];
+	    }
+	    
+	    private int get_total_wage(CompanyEmpWage companyEmpWage) 
+	    {   
+	    	int emp_hr = 0, total_hours = 0, total_days = 0;
 	    	
-	    	while(days<total_no_days && total_hr<=emp_hours)
-	    	{
-	    		int number=(int)Math.floor(Math.random()*10)%3;
-	    		if(full_time== number)
-			       emp_hr=8;
-			    else if(part_time== number)
-				   emp_hr=4;
-			    else
-				   emp_hr=0;
-			    total_hr+=emp_hr;
-			    days++;
+	        while (total_hours <= companyEmpWage.total_hours && total_days< companyEmpWage.total_days) 
+	        { 
+	          total_days++;
+	          int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
+	          switch (empCheck) 
+	          { 
+	              case part_time: 
+	        	     emp_hr=4; 
+	        	     break; 
+	        	  case full_time: 
+	        		 emp_hr=8; 
+	        		 break;
+	        	   default: 
+	        		 emp_hr=0; 
+	          }
+	          
+	    	  total_hours += emp_hr; 
 	    	}
-	    	return total_hr;
-	    }
-	    
-	    public void set_working_days(int total_days) 
-	    { 
-	      total_no_days=total_days; 
-	    }
-	    
-	    public void set_work_hours_month(int hr) 
-	    { 
-	      emp_hours=hr;
-	    }
-	    
-	    public void set_wage_per_hour(int wage) 
-	    { 
-	    	emp_wage_hr=wage; 
+	        return total_hours * companyEmpWage.emp_wage_hr; 
 	    } 
-}
-public class Employee 
-{
-	
+
 	public static void main(String[] args)
 	{
-		Employee_func company1=new Employee_func();
-		company1.set_working_days(20);
-		company1.set_work_hours_month(100);
-		company1.set_wage_per_hour(50);
-		
-		Employee_func company2=new Employee_func();
-		company2.set_working_days(20);
-		company2.set_work_hours_month(80);
-		company2.set_wage_per_hour(20);
-		
-		System.out.println("The number of hours employee1 worked :"+company1.get_total_hours());
-		System.out.println("Employee wage is : "+company1.get_total_wage());
-		System.out.println("The number of hours employee2 worked :"+company2.get_total_hours());
-		System.out.println("Employee wage is : "+company2.get_total_wage());
+		Employee emp_wage_build=new Employee();
+		emp_wage_build.add_CompanyWage("Vishal",30,3,15);
+		emp_wage_build.add_CompanyWage("Rel",20,4,10);
+		emp_wage_build.get_total_wage();
 	}
 }
 	    
